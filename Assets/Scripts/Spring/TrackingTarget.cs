74838ab7b19a1b1a5c977335122f6daf635ee0e3
叶凌峰 <yelingfeng@xmfunny.com>
2023-04-12 17:03:45 +0800

    <feature> spring test 1st

---
+++ Assets/Scripts/Spring/TrackingTarget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Spring {
+    public class TrackingTarget : MonoBehaviour {
+        public Vector3 prePos;
+        public Vector3 velocity;
+
+        private void Update() {
+            prePos = this.transform.position;
+
+            Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            this.velocity = GetComponent<Rigidbody>().velocity = 5 * dir;
+        }
+    }
+}
