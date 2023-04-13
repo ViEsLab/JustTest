cb1f489462c6faac73df58a9ada43e5bc4c0d16a
叶凌峰 <yelingfeng@xmfunny.com>
2023-04-13 16:22:18 +0800

    <misc> component access test

---
+++ Assets/Scripts/ComponentAccessTarget.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Spring {
+    public class ComponentAccessTarget : MonoBehaviour {
+        public Vector3 test = Vector3.one;
+
+        private void Update() {
+        }
+    }
+}
