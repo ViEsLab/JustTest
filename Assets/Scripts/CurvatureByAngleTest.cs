85edfe5a886751c5082666cd30c0530575a75fec
叶凌峰 <vieqqedf@gmail.com>
2023-04-14 16:52:42 +0800

    <feat> Curve Rotation Test

---
+++ Assets/Scripts/CurvatureByAngleTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+namespace Spring {
+    public class CurvatureByAngleTest : MonoBehaviour {
+        public GameObject startPoint;
+        public GameObject endPoint;
+        public GameObject curPoint;
+
+        private void Update() {
+            FuncFst();
+        }
+
+        private void FuncSnd() {
+            var curPos = curPoint.transform.position;
+            Vector3 fst = startPoint.transform.position - curPos;
+            Vector3 snd = endPoint.transform.position - curPos;
+
+            Vector3 foot = GetFootOfPerpendicular(
+                curPos, startPoint.transform.position, endPoint.transform.position);
+            Vector3 normalVec = foot - curPos;
+        }
+
+        private void FuncFst() {
+            var curPos = curPoint.transform.position;
+            Vector3 fst = startPoint.transform.position - curPos;
+            Vector3 snd = endPoint.transform.position - curPos;
+
+            Vector3 foot = GetFootOfPerpendicular(curPos, fst, snd);
+            curPoint.transform.up = -foot;
+        }
+
+        private Vector3 GetFootOfPerpendicular(Vector3 pt, Vector3 begin, Vector3 end) {
+            Vector3 retVal = Vector3.zero;
+
+            float dx = begin.x - end.x;
+            float dy = begin.y - end.y;
+            float dz = begin.z - end.z;
+            if (Mathf.Abs(dx) < float.Epsilon &&
+                Mathf.Abs(dy) < float.Epsilon &&
+                Mathf.Abs(dz) < float.Epsilon) {
+                retVal = begin;
+                return retVal;
+            }
+
+            float u = (pt.x - begin.x) * (begin.x - end.x) +
+                      (pt.y - begin.y) * (begin.y - end.y) +
+                      (pt.z - begin.z) * (begin.z - end.z);
+            u = u / (dx * dx + dy * dy + dz * dz);
+
+            retVal.x = begin.x + u * dx;
+            retVal.y = begin.y + u * dy;
+            retVal.y = begin.z + u * dz;
+
+            return retVal;
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlaneTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Spring {
+    public class PlaneTest : MonoBehaviour {
+        public GameObject fstPoint;
+        public GameObject secPoint;
+        public GameObject trdPoint;
+
+        private void Update() {
+            GetPlane(
+                fstPoint.transform.position,
+                secPoint.transform.position,
+                trdPoint.transform.position,
+                out float a, out float b, out float c, out float d);
+            Debug.Log(new Vector3(a, b, c));
+        }
+
+        public void GetPlane(Vector3 fst, Vector3 sec, Vector3 trd,
+            out float a, out float b, out float c, out float d) {
+            //                   |   i     j     k   |
+            // n = p1p2 x p1p3 = | x2-x1 y2-y1 z2-z1 | = a*i + b*j + c*k = (a, b, c)
+            //                   | x3-x1 y3-y1 z3-z1 |
+            a = (sec.y - fst.y) * (trd.z - fst.z) - (trd.y - fst.y) * (sec.z - fst.z);
+            b = (sec.z - fst.z) * (trd.x - fst.x) - (trd.z - fst.z) * (sec.x - fst.x);
+            c = (sec.x - fst.x) * (trd.y - fst.y) - (trd.x - fst.x) * (sec.y - fst.y);
+            d = -(a * fst.x + b * fst.y + c * fst.z);
+        }
+    }
+}
